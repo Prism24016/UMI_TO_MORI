6b1904942d8652d6da8375535202fa0e50655683
Prism【木村】 <kimura.mika035@gmail.com>
2025-04-25 20:42:14 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,21 +3,34 @@ $(function () {
   ハンバーガーメニュー
   ===================================================*/
   $(function () {
-    $("").on("click", function () {
-        $("").fadeToggle();
-        $("").toggleClass("");
+    // ハンバーガーメニューの開閉
+    $(".hamburger").click(function () {
+      $(".header").toggleClass("open");
     });
-});
-$(".hamburger").click(function () {
-    $(".header").toggleClass("open");
-});
 
-  $(function() {
-    setTimeout(function(){
+    // ハンバーガーメニュー内のリンクをクリックしたらメニューを閉じる
+    $(".header__hamburger__menu a").click(function () {
+      $(".header").removeClass("open");
+    });
+
+    // 最初のフェードイン・アウト
+    setTimeout(function () {
       $('.start p').fadeIn(1600);
-    },500);
-    setTimeout(function(){
+    }, 500);
+    setTimeout(function () {
       $('.start').fadeOut(500);
-    },2500);
+    }, 2500);
   });
+
+  if (!sessionStorage.getItem('visited')) {
+    $('.start p').fadeIn(1600);
+    setTimeout(function () {
+      $('.start').fadeOut(500);
+    }, 2500);
+
+    sessionStorage.setItem('visited', 'true');
+  } else {
+    $('.start').hide();
+  }
+
 });
